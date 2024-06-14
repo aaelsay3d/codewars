28d43bbe216e9f4c39ffaf2cfbea20931154fc80
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-06-14 10:23:51 +0300

    Add

---
+++ javascript/8-kyu/55a2d7ebe362935a210000b2.js
@@ -0,0 +1,9 @@
+function findSmallestInt(args) {
+  let smallest = Infinity;
+  for (let i = 0; i < args.length; i++){
+    if (args[i] < smallest) {
+      smallest = args[i];
+    }
+  }
+  return smallest;
+}
