581698875bef9705927f4183da08cec397aea6e7
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-20 16:46:15 +0300

    Add

---
+++ javascript/8-kyu/59dd3ccdded72fc78b000b25.js
@@ -0,0 +1,11 @@
+function whatday(num) { 
+  return {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday"
+  }[num] || "Wrong, please enter a number between 1 and 7";
+}
