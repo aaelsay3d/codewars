3f369b0ab54a887fad79864518b7e40470e32d79
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-20 17:10:47 +0300

    Add

---
+++ javascript/8-kyu/55a144eff5124e546400005a.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  getInfo(){
+    return `${this.name}s age is ${this.age}`;
+  }
+  
+  get info(){
+    return this.getInfo();
+  }
+}
