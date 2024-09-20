208f0fc2a97bb1f71347b65bf6b9a99a3aa8ac51
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-20 17:13:13 +0300

    Add

---
+++ javascript/8-kyu/56d49587df52101de70011e4.js
@@ -0,0 +1,11 @@
+function leo(oscar){
+  switch (oscar){
+    case 88:
+      return "Leo finally won the oscar! Leo is happy";
+    case 86:
+      return "Not even for Wolf of wallstreet?!";
+    default:
+      if(oscar<88) return "When will you give Leo an Oscar?";
+      return "Leo got one already!";
+  }
+}
